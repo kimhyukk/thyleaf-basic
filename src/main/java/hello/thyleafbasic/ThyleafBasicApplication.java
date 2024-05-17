f2f50159948010e2dc4376e5d89923874cf9e78a
Hyuk Kim <rlagur0222@gmail.com>
2024-05-17 15:25:54 +0900

    Initial commit

---
+++ src/main/java/hello/thyleafbasic/ThyleafBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.thyleafbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThyleafBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThyleafBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/thyleafbasic/ThyleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thyleafbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThyleafBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
